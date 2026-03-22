3ef48c5bd7ea00be98e9a6897e0998e5047e9b49
Daisyfarm <109863822+Daisyfarm@users.noreply.github.com>
2026-03-22 23:39:10 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// This is the ONLY thing that should be in this file
 export const sb = createClient(supabaseUrl, supabaseAnonKey);

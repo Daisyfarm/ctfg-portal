1a866c2c03571d51b155317b25439d72ad28f507
Daisyfarm <109863822+Daisyfarm@users.noreply.github.com>
2026-03-24 20:49:40 +0000

    Update supabase.ts

--- db/supabase.ts
+++ db/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// This check helps you see in the Vercel logs if the keys are missing
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("SUPABASE ERROR: Missing environment variables!");
+}
 
 export const sb = createClient(supabaseUrl, supabaseAnonKey);
